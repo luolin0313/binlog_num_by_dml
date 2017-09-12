c552b47644dc4a7d1e2d1fc86e330ad1284547e0
wuyongsheng <wuyongshenghub@163.com>
2017-09-12 09:43:16 +0800

    'rewrite'

--- binlog_num_by_dml.py
+++ binlog_num_by_dml.py
@@ -1,47 +1,37 @@
-#encoding:utf-8
+# encoding:utf-8
 
 import sys
-import subprocess
+import os
+import datetime
 
-def convert_mysqlbinlog_txt_format(starttime,endtime,filename):
-	# 二进制日志binlog转成文本格式binlog
-	cmdstr = "mysqlbinlog --base64-output=decode-rows -v -v --start-datetime='%s' --stop-datetime='%s' %s"%(starttime,endtime,filename)
-	file_binlog = subprocess.Popen(cmdstr)
-	# return file_binlog
-	handler_file_binlog(file_binlog)
 
-def handler_file_binlog(file_binlog):
-	# 过滤出insert/update/delete 
-	with open(file_binlog) as f:
-		for line in f:
-			line = line.strip()
-			if line.startswith(''):
-				group_by_dml(line)
-
-def group_by_dml(line):
-	# 统计dml在表上操作的次数
-	dml = dict()
-	if line not in dml:
-		dml[line] = dml.setdefault(line,1)
-	else:
-		dml[line] = dml.get(line)+1
-	return dml
-
-def top10():
-	# 筛选出对表最频繁dml
-	res = group_by_dml(line)
-	data = res.items()
-	print sorted(data,key=lambda data:data[1],reverse=True)[:10]
+def handler_binlog_to_dml_num(startime,endtime,filename):
+    # 定义字典存储每个表dml的次数
+    num_dml = dict()
+    cmd = "/usr/bin/mysqlbinlog --base64-output=decode-rows -v -v  --start-datetime='%s' --stop-datetime='%s' %s" % (
+        starttime, endtime, filename)
+    binlogtxtfile = os.popen(cmd).readlines()
+    for line in binlogtxtfile:
+        line =  line[3:].lstrip()
+        if line.startswith('UPDATE') or line.startswith('INSERT INTO') or line.startswith('DELETE FROM'):
+            line = (line.strip(),)
+            num_dml[line] = num_dml.get(line,0)+1
+    
+    #筛选出top10的dml的表
+    res = num_dml.items()
+    data = sorted(res,key=lambda res:res[1],reverse=True)[:10]
+    for k,v in data:
+        print "dml:%s-%s次数"%(k[0].replace('FROM','').replace('`',''),v)
 
 if __name__=="__main__":
-	if len(sys.argv)==1:
-		print 'python binlog_num_by_dml.py "2017-01-01 12:00:01" "2017-01-01 13:00:01" mysql-bin.000002'
-	elif sys.argv[1] == '' or sys.argv[2] == '' or sys.argv[3] == '':
-		print "缺少参数"
-	else:
-		convert_mysqlbinlog_txt_format(sys.argv[1],sys.argv[2],sys.argv[3])
-		print sys.argv[1:]
-
-
-
-
+    dt = datetime.datetime.now()
+    ds = datetime.datetime.strftime(dt,'%Y-%m-%d %H:%M:%S')
+    if len(sys.argv) == 1:
+        print "Usage: python %s '%s' '%s' %s"%(sys.argv[0],ds,ds,'mysql-bin.000001')
+    elif sys.argv[1] == '' or sys.argv[2] =='' or sys.argv[3] == '':
+        print "参数不能为空"
+    else:
+        starttime = sys.argv[1]
+        endtime = sys.argv[2]
+        filename = sys.argv[3]
+        handler_binlog_to_dml_num(starttime,endtime,filename)
